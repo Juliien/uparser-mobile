00d0bc58893bcee8d8744c33f5f52533bcb840de
valle <benoit.valleea@hotmail.fr>
2021-07-22 19:04:57 +0200

    clean(catalog): delete Log

--- app/src/main/java/com/esgi/uparser/catalog/CodeDetailActivity.kt
+++ app/src/main/java/com/esgi/uparser/catalog/CodeDetailActivity.kt
@@ -2,15 +2,10 @@ package com.esgi.uparser.catalog
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.annotation.RequiresApi
 import com.esgi.uparser.R
-import com.esgi.uparser.api.catalog.model.CodeResponse
 import kotlinx.android.synthetic.main.activity_code_detail.*
-import java.util.*
 
 class CodeDetailActivity : AppCompatActivity() {
 
@@ -32,9 +27,7 @@ class CodeDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_code_detail)
 
         val receivedCodeValue = intent?.getStringExtra("codeValue")
-        Log.d("Uparser", "Success encoded $receivedCodeValue")
         val decodedCodeValue = android.util.Base64.decode(receivedCodeValue, android.util.Base64.DEFAULT)
-        Log.d("Uparser", "Success decoded ${String(decodedCodeValue)}")
         codeContentPlaceHolder.text = String(decodedCodeValue)
     }
 }
