6b1dd208768d25c6631801eedce8f5c15d86c223
valle <benoit.valleea@hotmail.fr>
2021-07-22 14:46:39 +0200

    fix(login): fix login call and interface behaviour

---
+++ app/src/main/java/com/esgi/uparser/LoginActivity.kt
@@ -0,0 +1,87 @@
+package com.esgi.uparser
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.esgi.uparser.api.authentication.model.LoginModel
+import com.esgi.uparser.api.authentication.model.TokenResponse
+import com.esgi.uparser.api.authentication.service.AuthenticationService
+import com.esgi.uparser.api.provider.AppPreferences
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LoginActivity : AppCompatActivity() {
+
+    var isResultValid: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        AppPreferences.init(this)
+
+        val emailInput = findViewById<EditText>(R.id.outlined_email)
+        val passwordInput = findViewById<EditText>(R.id.outlined_password)
+        val submitButton = findViewById<Button>(R.id.login_submit)
+
+        submitButton.setOnClickListener {
+            val result = login(emailInput, passwordInput)
+        }
+    }
+    private fun login(emailInput: EditText, passwordInput: EditText): Boolean{
+        val email = emailInput.text.toString()
+        val password = passwordInput.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()
+            && email.isNotBlank() && password.isNotBlank()) {
+            val loginService = AuthenticationService()
+
+            loginService.login(LoginModel(email = email, password = password), object :
+                Callback<TokenResponse> {
+                override fun onResponse(call: Call<TokenResponse>, response: Response<TokenResponse>) {
+                    Log.d("Uparser", "Success login ${response.body()?.token}")
+                    if (response.body()?.token != null) {
+                        AppPreferences.token = response.body()?.token!!
+                        isResultValid = true
+                        UserDetailActivity.navigateTo(this@LoginActivity)
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "Login Failed ! ",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<TokenResponse>, t: Throwable) {
+
+                    Toast.makeText(
+                        applicationContext,
+                        "Server error ! ",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            })
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Missing value in fields ! ",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        return isResultValid
+    }
+
+    companion object {
+        fun navigateTo(context: Context) {
+            context.startActivity(Intent(context, LoginActivity::class.java))
+        }
+    }
+}
