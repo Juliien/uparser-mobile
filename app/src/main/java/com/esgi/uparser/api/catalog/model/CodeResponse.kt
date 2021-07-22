7cd7be52d59c6583e6e799ad31b2bcfffaa80c48
valle <benoit.valleea@hotmail.fr>
2021-07-22 14:41:58 +0200

    feat(res): add several resources

--- app/src/main/java/com/esgi/uparser/api/catalog/model/CodeResponse.kt
+++ app/src/main/java/com/esgi/uparser/api/catalog/model/CodeResponse.kt
@@ -1,12 +1,13 @@
-package com.esgi.uparser.api.catalog.model
+package com.esgi.uparser.api.code.model
 
 import com.google.gson.annotations.SerializedName
 
-data class CatalogModel(
+open class CodeResponse (
+    @SerializedName("id") val id: String,
     @SerializedName("userId") val userId: String,
     @SerializedName("codeEncoded") val codeEncoded: String,
     @SerializedName("extensionStart") val extensionStart: String,
     @SerializedName("extensionEnd") val extensionEnd: String,
     @SerializedName("language") val language: String,
-    @SerializedName("date") val date: String,
+    @SerializedName("date") val date: String
 )
